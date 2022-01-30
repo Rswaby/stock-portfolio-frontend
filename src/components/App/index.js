1340a0438ea6ee67a436f4c5528cfcb8aaa87525
Rohan Swaby <rohan_swaby51@yahoo.com>
2022-01-30 18:14:39 -0500

    add redirects

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -6,9 +6,7 @@ import LandingPage from '../Landing';
 import SignUpPage from '../SignUp';
 import SignInPage from '../SignIn';
 import PasswordForgetPage from '../PasswordForget';
-import HomePage from '../Home';
 import AccountPage from '../Account';
-import AdminPage from '../Admin';
 import StockPage from '../StockPage';
 import * as ROUTES from '../../constants/routes';
 import { AuthProvider } from '../Session/AuthContext';
@@ -23,9 +21,7 @@ const App = () => (
         <Route path={ROUTES.SIGN_IN} component={SignInPage} />
         <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
         <Route path={ROUTES.STOCKDETAILS} component={StockPage} />
-        {/* <Route path={ROUTES.HOME} component={HomePage} /> */}
         <Route path={ROUTES.ACCOUNT} component={AccountPage} />
-        {/* <Route path={ROUTES.ADMIN} component={AdminPage} /> */}
     </Router>
   </AuthProvider>
 );
